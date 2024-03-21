d3947de2c50468ab8e94531e790e7364a2d1ffd8
jl <joh.lilja@gmail.com>
2024-03-21 22:38:09 +0100

    plugins

---
+++ lua/user/init.lua
@@ -0,0 +1,7 @@
+require 'user.options'
+require 'user.lazy'
+require 'user.keymaps'
+require 'user.autocommands'
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et

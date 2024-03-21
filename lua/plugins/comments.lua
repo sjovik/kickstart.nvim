b23422ab57264e062de676392e4bd3e4f78681ba
jl <joh.lilja@gmail.com>
2024-03-21 22:08:18 +0100

    config

---
+++ lua/plugins/comments.lua
@@ -0,0 +1,7 @@
+-- Use `opts = {}` to force a plugin to be loaded.
+--
+--  This is equivalent to:
+--    require('Comment').setup({})
+
+-- "gc" to comment visual regions/lines
+return { 'numToStr/Comment.nvim', opts = {} }

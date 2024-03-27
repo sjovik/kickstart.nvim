5002262613182d3101ba9c89026d9e2beeb3c7eb
jl <joh.lilja@gmail.com>
2024-03-28 00:47:16 +0100

    lualine harpoon

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,20 @@ return {
     config = function()
       local harpoon = require 'harpoon'
       local list = harpoon:list()
-      local ui = require 'harpoon.list'
+
+      local function get_current_mark()
+        local root_dir = list.config:get_root_dir()
+        local current_file_path = vim.api.nvim_buf_get_name(0)
+        for i = 1, list:length() do
+          local list_item_path = list:get(i).value
+          local full_path = root_dir .. '/' .. list_item_path
+
+          if full_path == current_file_path then
+            return tostring(i)
+          end
+        end
+        return '-'
+      end
 
       local function harpoon_component()
         local total_marks = #list.items
@@ -14,14 +27,9 @@ return {
           return ''
         end
 
-        local current_mark = '-'
+        local current_mark = get_current_mark()
 
-        local mark_i = 2
-        if mark_i ~= nil then
-          current_mark = tostring(mark_i)
-        end
-
-        return string.format(' %s/%d', current_mark, total_marks)
+        return string.format('󱡅 %s/%d', current_mark, total_marks)
       end
 
       require('lualine').setup {
@@ -33,6 +41,7 @@ return {
         },
         sections = {
           lualine_b = {
+            { 'branch', icon = '' },
             harpoon_component,
             'diff',
             'diagnostics',
